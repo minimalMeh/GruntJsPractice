c32c859420bb0ca04221007e45183210a857b4ce
minimalMeh <dasha.osd@gmail.com>
2020-09-30 17:28:57 +0300

    remove comment

--- ConvertJsES6toES5/Gruntfile.js
+++ ConvertJsES6toES5/Gruntfile.js
@@ -14,19 +14,6 @@ module.exports = function(grunt) {
                     "ext": "-compiled.js"
                 }]
             }
-        //task0: {
-        //  files: [{
-        //    "expand": true,
-        //    "src": ["dist/calculator.js"],
-        //    "dest": "js-compiled",
-        //    "ext": "-compiled.js"
-        //   }],
-        //   options: {
-        //      transform: [
-        //        ['babelify', { presets: "es2015" }]
-        //      ]
-        //   }
-        
         }
     });
     grunt.loadNpmTasks('grunt-babel');
